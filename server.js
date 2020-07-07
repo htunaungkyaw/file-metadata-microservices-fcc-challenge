7c6dcba98de2da8a52304a4c9adad589a13c7afa
HtunAungKyaw <htunaungkyaw.1997.jan.19@gmail.com>
2020-07-07 08:24:28 +0630

    done

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/hello", function (req, res) {
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   res.json({
-    name: req.file.filename,
+    name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size,
   });
